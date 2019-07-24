5a8921c48875cf8921b9903ade771e6625b21da9
Azuki291 <53243975+Azuki291@users.noreply.github.com>
2019-07-24 05:14:45 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -30,3 +30,44 @@ bot.on('message', message => {
         message.reply("Création du bot le 23/07/2019 à 23 : 06 : 24");
     }
 });
+
+
+bot.login(config.token);
+
+bot.on('message', message => {
+    let command = message.content.split(" ")[0];
+    const args = message.content.slice(prefix.lenght).split(/ + /);
+    command = args.shift().toLowercase();
+
+if (command === "kick") {
+    let modRole = message.guild.roles.find("name", "Test");
+    if(!message.member.roles.has(modRole.id)) {
+        return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+    }
+    if(message.mentions.users.size === 0) {
+        return message.reply("Merci de mentionner l'utulisateur à expulser.").catch(console.error);
+    }  
+    let kickMember = message.guild.member(message.mentions.users.first());
+    if(!kickMember) {
+        return message.reply("Cet utilisateur est introuvable ou impossible à expulser.")
+    }
+    if(!message.guild.member(bot.user).hasPermission("KICK MEMBERS")) {
+        return message.reply("Je n'ai pas les permissions KICK_MEMBERS pour faire ceci.").catch(console.error);
+    }
+    kickMember.kick().then(member => {
+        message.reply(`${member.user.username} a été expulser avec succès.`).catch(console.error);
+    }).catch(console.error);
+
+}
+
+if (command === "ban") {
+    let modRole = message.guild.roles.find("name", "Test");
+    if(!message.member.roles.has(modRole.id)) {
+        return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+    }
+    const member = message.mentions.members.first();
+    if (!member) return message.reply("Merci de mentionner l'utilisateur devant être puni du banhammer");
+    member.ban().then(member => {
+        message.reply(`${member.user.username} a pris le banhammer de plein fouet.`).catch(console.error);
+    }).catch(console.error)
+}});
